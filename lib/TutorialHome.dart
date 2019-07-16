d9895705a5be7ad547c80f320e869aad1383643d
maobui <mao.buidinh@hotmail.com>
2019-07-16 16:52:02 +0700

    - add increment.

--- lib/TutorialHome.dart
+++ lib/TutorialHome.dart
@@ -16,9 +16,63 @@ class TutorialHome extends StatelessWidget {
               icon: Icon(Icons.search), tooltip: 'Search menu', onPressed: null)
         ],
       ),
-      body: Center(child: Text("Hello,  world!")),
+      body: Center(child: Counter()),
       floatingActionButton: FloatingActionButton(
           tooltip: 'Add', child: Icon(Icons.add), onPressed: null),
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  final int count;
+
+  CounterDisplay({this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('Count : $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  CounterIncrementor({this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+
+}
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter ++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment),
+        CounterDisplay(count: _counter),
+      ],
     );
   }
 }
