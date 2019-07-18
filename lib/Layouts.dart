6bc5f478c4bfe38d114146ef1978e8951758c516
maobui <mao.buidinh@hotmail.com>
2019-07-18 14:53:21 +0700

    - gridview, listview.

--- lib/Layouts.dart
+++ lib/Layouts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Layouts extends StatelessWidget {
+  static final _useGridView = false;
   @override
   Widget build(BuildContext context) {
     final titleText = Container(
@@ -136,7 +137,7 @@ class Layouts extends StatelessWidget {
         title: Text("Layouts example"),
       ),
       body: Center(
-        child: Container(
+/*        child: Container(
           margin: EdgeInsets.fromLTRB(0, 8, 0, 8),
           height: 600,
           child: Card(
@@ -154,9 +155,10 @@ class Layouts extends StatelessWidget {
 //                ),
 //              ],
 //            ),
-          child: _buildImageColumn(),
+            child: _buildImageColumn(),
           ),
-        ),
+        ),*/
+        child: _useGridView ? _buildGrid() : _buildList(),
       ),
     );
   }
@@ -236,32 +238,74 @@ class Layouts extends StatelessWidget {
       );
 
   Widget _buildDecoratedImage(int imageIndex) => Expanded(
-      child: Container(
+          child: Container(
         decoration: BoxDecoration(
           border: Border.all(width: 10, color: Colors.black38),
           borderRadius: const BorderRadius.all(const Radius.circular(8)),
         ),
         margin: const EdgeInsets.all(4),
         child: Image.asset('images/pic$imageIndex.jpg'),
-      )
-  );
+      ));
 
   Widget _buildImageRow(int imageIndex) => Row(
-    children: [
-      _buildDecoratedImage(imageIndex),
-      _buildDecoratedImage(imageIndex + 1),
-    ],
-  );
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
+        ],
+      );
 
   Widget _buildImageColumn() => Container(
-    decoration: BoxDecoration(
-      color: Colors.black26,
-    ),
-    child: Column(
-      children: [
-        _buildImageRow(1),
-        _buildImageRow(3),
-      ],
-    ),
-  );
+        decoration: BoxDecoration(
+          color: Colors.black26,
+        ),
+        child: Column(
+          children: [
+            _buildImageRow(1),
+            _buildImageRow(3),
+          ],
+        ),
+      );
+
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(30));
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count, (i) => Container(child: Image.asset('images/pic$i.jpg')));
+
+  Widget _buildList() => ListView(
+        children: [
+          _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+          _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+          _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+          _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+          _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+              Icons.theaters),
+          _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+          Divider(),
+          _tile('Kescaped_code#39;s Kitchen', '757 Monterey Blvd',
+              Icons.restaurant),
+          _tile('Emmyescaped_code#39;s Restaurant', '1923 Ocean Ave',
+              Icons.restaurant),
+          _tile(
+              'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+          _tile('La Ciccia', '291 30th St', Icons.restaurant),
+        ],
+      );
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+        title: Text(title,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+            )),
+        subtitle: Text(subtitle),
+        leading: Icon(
+          icon,
+          color: Colors.blue[500],
+        ),
+      );
 }
