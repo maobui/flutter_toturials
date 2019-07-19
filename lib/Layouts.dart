60dfe4dd625c0f7394ba9578b381ff25aeb38794
maobui <mao.buidinh@hotmail.com>
2019-07-19 10:24:31 +0700

    - plug the stateful widget into the widget tree

--- lib/Layouts.dart
+++ lib/Layouts.dart
@@ -161,11 +161,7 @@ class Layouts extends StatelessWidget {
           ),
         ),
         /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+        FavoriteWidget(),
       ],
     ),
   );
@@ -492,4 +488,49 @@ class Layouts extends StatelessWidget {
           ),
         ),
       );
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FavoriteWidget();
+}
+
+class _FavoriteWidget extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
 }
